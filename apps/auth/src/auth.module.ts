22b5781225c0ea4f3d7b5397e80bf87bfe21e1d3
toughCircle <toughCircle@gmail.com>
2024-10-28 17:04:39 +0900

    인증 서버 가드 제거

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -10,7 +10,6 @@ import { CommonService } from '@app/common';
 import { DBConfigModule } from './config/db-config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/user.model';
-import { JwtMiddleware } from './middleware/jwt.middleware';
 import { UserService } from './services/user.service';
 import { JwtManager } from './services/jwt-manager.service';
 import { JwtConfigModule } from './config/jwt-config.module';
@@ -25,15 +24,4 @@ import { JwtConfigModule } from './config/jwt-config.module';
   providers: [AuthService, CommonService, UserService, JwtManager],
   exports: [UserService, TypeOrmModule],
 })
-export class AuthModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .exclude(
-        { path: 'auth/login', method: RequestMethod.POST },
-        { path: 'auth/register', method: RequestMethod.POST },
-        { path: 'auth/refresh', method: RequestMethod.POST },
-      )
-      .forRoutes('*');
-  }
-}
+export class AuthModule {}
