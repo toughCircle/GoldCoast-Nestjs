d9b36332b0d643301ff53ac104137185fa592e24
toughCircle <toughCircle@gmail.com>
2024-10-29 15:09:57 +0900

    파싱 조건 추가

--- apps/resource/src/services/gold-price.service.ts
+++ apps/resource/src/services/gold-price.service.ts
@@ -54,22 +54,28 @@ export class GoldPriceService {
   // API 응답에서 필요한 금 시세 추출
   extractPriceFromResponse(goldPriceResponse: any, itemType: string): number {
     try {
-      console.log('Parsed response:', goldPriceResponse);
+      // JSON 파싱 여부 확인
+      const response =
+        typeof goldPriceResponse === 'string'
+          ? JSON.parse(goldPriceResponse)
+          : goldPriceResponse;
+
+      console.log('Parsed response:', response);
 
       let pricePerGram = 0;
 
       switch (itemType) {
         case 'GOLD_24':
-          pricePerGram = Number(goldPriceResponse.price_gram_24k);
+          pricePerGram = Number(response.price_gram_24k);
           break;
         case 'GOLD_22':
-          pricePerGram = Number(goldPriceResponse.price_gram_22k);
+          pricePerGram = Number(response.price_gram_22k);
           break;
         case 'GOLD_21':
-          pricePerGram = Number(goldPriceResponse.price_gram_21k);
+          pricePerGram = Number(response.price_gram_21k);
           break;
         case 'GOLD_18':
-          pricePerGram = Number(goldPriceResponse.price_gram_18k);
+          pricePerGram = Number(response.price_gram_18k);
           break;
         default:
           throw new BadRequestException(`Unsupported item type: ${itemType}`);
