e361f65badd439a128dc0dd5a98f9c9d1cb12c8c
toughCircle <toughCircle@gmail.com>
2024-11-06 15:48:16 +0900

    트랜젝션 문제 해결 시도_1

--- apps/resource/src/main.ts
+++ apps/resource/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ResourceModule } from './resource.module';
-import { initializeTransactionalContext } from 'typeorm-transactional';
+import {
+  initializeTransactionalContext,
+  addTransactionalDataSource,
+} from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   // 트랜잭션 컨텍스트 초기화
   initializeTransactionalContext();
 
   const app = await NestFactory.create(ResourceModule);
+
+  // 트랜잭션에 사용할 데이터 소스를 추가
+  const dataSource = app.get(DataSource);
+  addTransactionalDataSource(dataSource);
 
   app.listen(4001, () => {
     console.log('HTTP Server is running on http://localhost:4001');
