d296220edfb2f8c716174ad5074ce46f36a24107
toughCircle <toughCircle@gmail.com>
2024-11-06 15:32:36 +0900

    트랜잭션 컨텍스트 초기화

--- apps/resource/src/main.ts
+++ apps/resource/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ResourceModule } from './resource.module';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
+  // 트랜잭션 컨텍스트 초기화
+  initializeTransactionalContext();
+
   const app = await NestFactory.create(ResourceModule);
 
   app.listen(4001, () => {
